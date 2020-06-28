8575159414073b4e599e17b39da205d7a8ea93bb
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-06-28 12:44:48 +0200

    form part 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Person from "./Person";
+import AddPerson from "./AddPerson";
 // container component
 class App extends React.Component {
+  state = {
+    list: [
+      { id: 1, name: "ahmed", age: 27, belt: "black" },
+      { id: 2, name: "ali", age: 30, belt: "green" },
+      { id: 3, name: "mohamed", age: 20, belt: "yellow" },
+    ],
+  };
   render() {
-    this.state = {
-      list: [
-        { id: 1, name: "ahmed", age: 27, belt: "black" },
-        { id: 2, name: "ali", age: 30, belt: "green" },
-        { id: 3, name: "mohamed", age: 20, belt: "yellow" },
-      ],
-    };
     return (
       <div className="App">
         <h1>React App</h1>
         <p>Welcome :)</p>
         <Person personsList={this.state.list} />
+        <AddPerson />
       </div>
     );
   }
