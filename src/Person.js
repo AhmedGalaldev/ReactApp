893a00e8db2c160c32cd3c7f0c64c3d0d8423cbd
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-06-28 12:17:53 +0200

    use conditional if and ternary operator

--- src/Person.js
+++ src/Person.js
@@ -1,16 +1,33 @@
 import React from "react";
 // ui component
 const Person = ({ personsList }) => {
-  const getList = personsList.map((person) => {
-    return (
-      <div className="person" key={person.id}>
-        <h3>Name: {person.name}</h3>
-        <h3>Age: {person.age}</h3>
-        <h3>Belt: {person.belt}</h3>
-      </div>
-    );
-  });
-  return <div>{getList}</div>;
+  // const getList = personsList.map((person) => {
+  //   if (person.age > 20) {
+  //     return (
+  //       <div className="person" key={person.id}>
+  //         <h3>Name: {person.name}</h3>
+  //         <h3>Age: {person.age}</h3>
+  //         <h3>Belt: {person.belt}</h3>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // });
+
+  return (
+    <div>
+      {personsList.map((person) => {
+        return person.age > 20 ? (
+          <div className="person" key={person.id}>
+            <h3>Name: {person.name}</h3>
+            <h3>Age: {person.age}</h3>
+            <h3>Belt: {person.belt}</h3>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Person;
